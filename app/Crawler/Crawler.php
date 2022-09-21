da79743735c0386504cb2306739e81cc57b60e4e
mike <mikewang890808@gmail.com>
2022-09-22 00:57:26 +0800

    update Crawler class to follow PSR(using PHP-CS-FIXER)

--- app/Crawler/Crawler.php
+++ app/Crawler/Crawler.php
@@ -1,33 +1,40 @@
 <?php
 
 namespace App\Crawler;
-use GuzzleHttp\Client as HttpClient;
 
+use GuzzleHttp\Client as HttpClient;
 
 /**
  * Crawl agencyanalytics website and parse, calculate several result value.
- * Entry Method is startCrawl()
- * 
+ * Entry method is startCrawl().
+ *
  * Entry URL parameter is get from .env file
  */
 class Crawler
 {
-    private $base_url;
-    private $max_crawl = 6;
+    private $baseUrl;
 
-    public $internal_links = [];
-    public $external_links = [];
+    const MAX_CRAWL = 6;
+
+    public $internalLinks = [];
+
+    public $externalLinks = [];
+
     public $images = [];
-    public $number_of_pages;
 
-    public $avg_page_load;
-    public $avg_word_count;
-    public $avg_title_length;
+    public $numberOfPages;
+
+    public $avgPageLoad;
+
+    public $avgWordCount;
+
+    public $avgTitleLength;
+
     public $pages;
 
     public function __construct()
     {
-        $this->base_url = env('CRAWL_URL');
+        $this->baseUrl = env('CRAWL_URL');
     }
 
     // Entry Point Method
@@ -36,30 +43,36 @@ class Crawler
         $pages = [];
         $url = '/';
 
-        // crawl children pages until reach out to value of max_crawl
-        for ($l = 0; $l < $this->max_crawl; $l ++) {
+        // crawl children pages until reach out to value of MAX_CRAWL
+        for ($l = 0; $l < self::MAX_CRAWL; $l++) {
             $pages = $this->crawlPage($url, $pages);
-        
-            for ($i = 0; $i < count($this->internal_links); $i ++) {
-                $url = $this->internal_links[$i];
-                if (!array_key_exists($url, $pages)) {
+
+            for ($i = 0; $i < count($this->internalLinks); $i++) {
+                $url = $this->internalLinks[$i];
+                if (! array_key_exists($url, $pages)) {
                     break;
                 }
             }
         }
-        
+
         $urls = array_keys($pages);
-        $this->number_of_pages = count($urls);
+        $this->numberOfPages = count($urls);
 
         // Calculate several result value and save to member variable.
-        $this->avg_page_load = array_sum(array_map(function($url) use ($pages) { return $pages[$url]['load_time']; }, $urls)) / $this->number_of_pages;
-        $this->avg_word_count = array_sum(array_map(function($url) use ($pages) { return $pages[$url]['word_count']; }, $urls)) / $this->number_of_pages;
-        $this->avg_title_length = array_sum(array_map(function($url) use ($pages) { return strlen($pages[$url]['title']); }, $urls)) / $this->number_of_pages;
+        $this->avgPageLoad = array_sum(array_map(function ($url) use ($pages) {
+            return $pages[$url]['load_time'];
+        }, $urls)) / $this->numberOfPages;
+        $this->avgWordCount = array_sum(array_map(function ($url) use ($pages) {
+            return $pages[$url]['word_count'];
+        }, $urls)) / $this->numberOfPages;
+        $this->avgTitleLength = array_sum(array_map(function ($url) use ($pages) {
+            return strlen($pages[$url]['title']);
+        }, $urls)) / $this->numberOfPages;
         $this->pages = $pages;
     }
 
     /**
-     * Crawling one page
+     * Crawling one page.
      *
      * @param [string] $url
      * @param [array] $pages
@@ -69,8 +82,8 @@ class Crawler
     {
         $client = new HttpClient();
         $start = time();
-        $res = $client->get($this->base_url . $url);
-        
+        $res = $client->get($this->baseUrl.$url);
+
         $status = $res->getStatusCode(); // 200
         $body = $res->getBody();
 
@@ -78,8 +91,8 @@ class Crawler
         $end = time();
         $page['load_time'] = $end - $start;
 
-        $this->external_links = array_unique(array_merge($this->external_links, $page['external_links']));
-        $this->internal_links = array_unique(array_merge($this->internal_links, $page['internal_links']));
+        $this->externalLinks = array_unique(array_merge($this->externalLinks, $page['externalLinks']));
+        $this->internalLinks = array_unique(array_merge($this->internalLinks, $page['internalLinks']));
         $this->images = array_unique(array_merge($this->images, $page['images']));
 
         $pages[$url] = [
@@ -99,42 +112,44 @@ class Crawler
      * @param [type] $body
      * @return void
      */
-    private function parseBody($url, $body) {
+    private function parseBody($url, $body)
+    {
         $page = [
-            'internal_links' => [],
-            'external_links' => [],
+            'internalLinks' => [],
+            'externalLinks' => [],
             'images' => [],
             'title' => '',
             'word_count' => 0,
             'load_time' => 0,
         ];
         $exp_link = "/<a[^>]+href=['\"]([^'\"]+)['\"]/i";
-        preg_match_all($exp_link, $body, $matches); 
-        
+        preg_match_all($exp_link, $body, $matches);
+
         if (count($matches) > 0) {
-            $links = array_map(function($link) {
-                $link = preg_replace("/#.+$/i", "", $link);
+            $links = array_map(function ($link) {
+                $link = preg_replace('/#.+$/i', '', $link);
                 if ($link == '') {
                     $link = '/';
                 }
+
                 return $link;
             }, $matches[1]);
             $links = array_unique($links);
-            $page['internal_links'] = array_filter($links, function($link, $v) use ($url) {
-                return preg_match("/^http/i", $link) == 0 && $link != $url;
+            $page['internalLinks'] = array_filter($links, function ($link, $v) use ($url) {
+                return preg_match('/^http/i', $link) == 0 && $link != $url;
             }, ARRAY_FILTER_USE_BOTH);
-            
-            $page['external_links'] = array_filter($links, function($link, $v) use ($url) {
-                return preg_match("/^http/i", $link) > 0 && $link != $url;
+
+            $page['externalLinks'] = array_filter($links, function ($link, $v) use ($url) {
+                return preg_match('/^http/i', $link) > 0 && $link != $url;
             }, ARRAY_FILTER_USE_BOTH);
         }
-        
+
         $exp_img = "/<img[^>]+src=['\"]([^'\"]+)['\"]/i";
-        preg_match_all($exp_img, $body, $matches); 
+        preg_match_all($exp_img, $body, $matches);
         if (count($matches) > 0) {
             $images = array_unique($matches[1]);
-            $page['images'] = array_filter($images, function($img, $v) {
-                return preg_match("/^data-image/i", $img) == 0;
+            $page['images'] = array_filter($images, function ($img, $v) {
+                return preg_match('/^data-image/i', $img) == 0;
             }, ARRAY_FILTER_USE_BOTH);
         }
         $exp_title = "/<title[^>]*>(.+)<\/title>/i";
@@ -143,10 +158,10 @@ class Crawler
             $page['title'] = $matches[1];
         }
 
-        $options = array(
+        $options = [
             'ignore_errors' => true,
-            'drop_links' => true
-        );
+            'drop_links' => true,
+        ];
         $text = \Soundasleep\Html2Text::convert($body, $options);
         $page['word_count'] = str_word_count($text);
 
--- app/Http/Controllers/CrawlController.php
+++ app/Http/Controllers/CrawlController.php
@@ -18,13 +18,13 @@ class CrawlController extends BaseController
         $crawler->startCrawl();
 
         return view('crawl.index', [
-            'number_of_pages' => $crawler->number_of_pages,
+            'number_of_pages' => $crawler->numberOfPages,
             'number_of_images' => count($crawler->images),
-            'number_of_internal_links' => count($crawler->internal_links),
-            'number_of_external_links' => count($crawler->external_links),
-            'avg_page_load' => $crawler->avg_page_load,
-            'avg_word_count' => $crawler->avg_word_count,
-            'avg_title_length' => $crawler->avg_title_length,
+            'number_of_internal_links' => count($crawler->internalLinks),
+            'number_of_external_links' => count($crawler->externalLinks),
+            'avg_page_load' => $crawler->avgPageLoad,
+            'avg_word_count' => $crawler->avgWordCount,
+            'avg_title_length' => $crawler->avgTitleLength,
             'pages' => $crawler->pages
         ]);
     }
